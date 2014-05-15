7aac19ed5aeab0a626d2f234956d8b62c63352dd
abreun68 <nasser.abreu@gmail.com>
2014-05-14 20:28:01 -0400

    More cleanup

--- src/edu/rowan/algorithms/BruteForceSolver.java
+++ src/edu/rowan/algorithms/BruteForceSolver.java
@@ -22,63 +22,62 @@ public class BruteForceSolver {
 
     public void generatePermutations() {
         int lastItemIndex = cities.size() - 1;
-        System.out.println(cities.toString() + " Dist.:" +  getTotalDistance(cities));        
+        System.out.println(cities.toString() + " Dist.:" + getTotalDistance(cities));
         for (int j = 0; j < cities.size(); j++) {
-            
+
             for (int trail = lastItemIndex; trail >= 0; trail--) {
 
-            	int head = (trail - 1);
-            	//System.out.println(head + " " + trail);
+                int head = (trail - 1);
+                //System.out.println(head + " " + trail);
                 if (head >= 0) {
                     if (cities.get(trail) > cities.get(head)) {
                         //System.out.println("Choosing head " + cities.get(head) + " is in slot " + head);
                         int lastSlot = lastItemIndex;
                         while (cities.get(head) > cities.get(lastSlot)) {
-                        	lastSlot--;
+                            lastSlot--;
                         }
                         //System.out.println("Choosing lastSlot " + cities.get(lastSlot));
                         //System.out.println("Swapping " + cities.get(head) + " with " + cities.get(lastSlot));
                         swap(lastSlot, head);
                         //System.out.println("Calling Sort with " + head);
-                        sort(head);     
+                        sort(head);
                         //System.out.println("lastItemIndex = " + lastItemIndex);
                         trail = lastItemIndex + 1;
-                        System.out.println(cities.toString() + " Dist.:" +  getTotalDistance(cities));                                
+                        System.out.println(cities.toString() + " Dist.:" + getTotalDistance(cities));
                     }
                 }
             }
         }
     }//end of generatePermutations()
-    
+
     private void sort(int m) {
         //last item index
         int index = (cities.size() - 1);
-        int n = m; 
+        int n = m;
         //System.out.println("head = "+ m + "; index = " + index);
         for (int i = m + 1; i < index; i++) {
-        	for (int j = n + 1; j < index; j++) {
-        		if (cities.get(j) > cities.get(j+1)) {
-        			swap(j, (j + 1));
-        		}
-        	}
+            for (int j = n + 1; j < index; j++) {
+                if (cities.get(j) > cities.get(j + 1)) {
+                    swap(j, (j + 1));
+                }
+            }
         }
     }//end of sort()
-    
+
     private double getFactorial(int n) {
         double fact = 1;
         for (int i = 1; i <= n; i++) {
             fact = fact * i;
         }
         return fact;
-    }
-    
-    private void swap(int j, int k){
+    }//end of getFactorial()
+
+    private void swap(int j, int k) {
         Integer tmp = cities.get(j);
         cities.set(j, cities.get(k));
         cities.set(k, tmp);
-    }
-    
- 
+    }//end of swap()
+
     private double getTotalDistance(ArrayList<Integer> cities) {
         double total = 0.0;
         int x1, y1, x2, y2;
@@ -101,7 +100,6 @@ public class BruteForceSolver {
         }// end for loop
 
         return total;
-    }
-    
-    
-}
+    }// end of getTotalDistance()
+
+}//end of class
