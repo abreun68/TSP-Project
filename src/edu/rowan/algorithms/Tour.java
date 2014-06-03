64dae781e8af11f53427f548af99057b62b68d22
Nasser Abreu <nasser.abreu@gmail.com>
2014-06-03 00:39:00 -0400

    Fixed issue with files without a blank comment.

--- src/edu/rowan/algorithms/Tour.java
+++ src/edu/rowan/algorithms/Tour.java
@@ -52,8 +52,9 @@ public class Tour {
 
         if (line.contains("COMMENT")) {
             String[] split = line.split(":");
-            if (split.length > 1)
-            this.comment = split[1].trim();
+            if (split.length > 1) {
+                this.comment = split[1].trim();
+            }
         }
 
         if (line.contains("TYPE")) {
